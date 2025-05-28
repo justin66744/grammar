c9a72d148a478053a60888aeb321891a21f030d7
justin66744 <justintt66744@gmail.com>
2025-05-28 02:56:01 -0700

    -Implemented rest of main file for functionality

--- project4.py
+++ project4.py
@@ -3,16 +3,25 @@
 # ICS 33 Spring 2025
 # Project 4: Still Looking for Something
 
+from parsing import *
+
 def read_inp():
     inp_file = input()
     sentences = int(input())
     start_var = input()
-
     return inp_file, sentences, start_var
 
+def generate_sentence(grammar, start_var):
+    pieces = list(grammar.generate(start_var))
+    return ' '.join(pieces)
 
 def main() -> None:
-    pass
+    file, sentences, start_var = read_inp()
+    res = parse_file(file)
+
+    for x in range(sentences):
+        sentence = generate_sentence(res, start_var)
+        print(sentence)
 
 
 if __name__ == '__main__':
